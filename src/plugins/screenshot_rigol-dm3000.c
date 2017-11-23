c0a4199c50f819e069d96285b7de89be927b8cb6
Martin Lund <martin.lund@keep-it-simple.com>
2017-11-23 18:26:01 +0100

    Add various Rigol screenshot plugins
    Add the following Rigol screenshot plugins:
    
    rigol-dg4000   Rigol DG4000 series function generator
    rigol-dm3000   Rigol DM3000 series digital multimeter
     rigol-dp800   Rigol DP800 series power supply
    rigol-dsa800   Rigol DSA800 series spectrum analyzer
    rigol-dsa700   Rigol DSA700 series spectrum analyzer
    
    The code is added on behalf of PeDre from the EEVBlog forum.

---
+++ src/plugins/screenshot_rigol-dm3000.c
@@ -0,0 +1,91 @@
+/*
+ * Copyright (c) 2017  Martin Lund
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ * 3. Neither the name of the copyright holders nor contributors may be
+ *    used to endorse or promote products derived from this software
+ *    without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+ * A PARTICULAR PURPOSE ARE DISCLAIMED.  IN NO EVENT SHALL THE COPYRIGHT
+ * HOLDERS OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <string.h>
+#include <ctype.h>
+#include <lxi.h>
+#include "error.h"
+#include "screenshot.h"
+
+#define IMAGE_SIZE_MAX 0x400000 // 4 MB
+
+int rigol_dm3000_screenshot(char *address, int timeout)
+{
+    char response[IMAGE_SIZE_MAX];
+    char *command, *image;
+    int device, length, n;
+    char c;
+
+    // Connect to LXI instrument
+    device = lxi_connect(address, 0, NULL, timeout, VXI11);
+    if (device == LXI_ERROR)
+    {
+        error_printf("Failed to connect\n");
+        return 1;
+    }
+
+    // Send SCPI command to grab BMP image
+    command = ":DISP:DATA?";
+    lxi_send(device, command, strlen(command), timeout);
+    length = lxi_receive(device, response, IMAGE_SIZE_MAX, timeout);
+    if (length < 0)
+    {
+        error_printf("Failed to receive message\n");
+        return 1;
+    }
+
+    // Strip TMC block header
+    c = response[1];
+    n = atoi(&c);
+    image = &response[0];
+    image += n+2;
+    length -= n+2;
+
+    // Dump remaining BMP image data to file
+    screenshot_file_dump(image, length, "bmp");
+
+    // Disconnect
+    lxi_disconnect(device);
+
+    return 0;
+}
+
+// Screenshot plugin configuration
+struct screenshot_plugin rigol_dm3000 =
+{
+    .name = "rigol-dm3000",
+    .description = "Rigol DM3000 series digital multimeter",
+    .regex = "RIGOL TECHNOLOGIES Rigol Technologies DM3...",
+    .screenshot = rigol_dm3000_screenshot
+};
--- src/screenshot.c
+++ src/screenshot.c
@@ -49,6 +49,11 @@
 extern struct screenshot_plugin keysight_iv2000x;
 extern struct screenshot_plugin rigol_1000;
 extern struct screenshot_plugin rigol_2000;
+extern struct screenshot_plugin rigol_dg4000;
+extern struct screenshot_plugin rigol_dm3000;
+extern struct screenshot_plugin rigol_dp800;
+extern struct screenshot_plugin rigol_dsa700;
+extern struct screenshot_plugin rigol_dsa800;
 extern struct screenshot_plugin rs_hmo1000;
 extern struct screenshot_plugin siglent_sds1000x;
 extern struct screenshot_plugin siglent_ssa3000x;
@@ -197,6 +202,11 @@ void screenshot_register_plugins(void)
     screenshot_plugin_register(&keysight_iv2000x);
     screenshot_plugin_register(&rigol_1000);
     screenshot_plugin_register(&rigol_2000);
+    screenshot_plugin_register(&rigol_dg4000);
+    screenshot_plugin_register(&rigol_dm3000);
+    screenshot_plugin_register(&rigol_dp800);
+    screenshot_plugin_register(&rigol_dsa700);
+    screenshot_plugin_register(&rigol_dsa800);
     screenshot_plugin_register(&rs_hmo1000);
     screenshot_plugin_register(&siglent_sds1000x);
     screenshot_plugin_register(&siglent_ssa3000x);
