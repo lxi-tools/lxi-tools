58eb4f258c0c674e20ef1ee7fe49ee7016024073
Martin Lund <martin.lund@keep-it-simple.com>
2018-03-17 11:14:46 +0100

    Update basic-tests.lua

--- test/basic-tests.lua
+++ test/basic-tests.lua
@@ -1,18 +1,45 @@
--- Basic Lua tests
+-- Basic Lua tests --
 
 
+-- To run tests simply do:
+-- $ lxi run basic-tests.lua
 
--- Print
+
+--[[
+
+Why Lua?
+
+Lua provides a set of unique features that makes it distinct from other
+languages. These include:
+
+ * Extensible
+ * Simple
+ * Efficient
+ * Portable
+ * Free and open
+
+Official Lua documentation:
+http://www.lua.org/docs.html
+
+Lua quick guide:
+https://www.tutorialspoint.com/lua/lua_quick_guide.htm
+
+Lua tutorial:
+https://www.tutorialspoint.com/lua/index.htm
+
+--]]
+
+
+-- Print --
 a = 42
-print("Hello world")
+print("Hello lxi-tools")
 print("Value of a is " .. a)
-print("Lua version " .. _VERSION)
+print("Running " .. _VERSION)
+print("Is " .. a .. " the answer?")
 
 
-
--- Conditional
+-- Conditional --
 a = true
-
 if (a)
 then
    print("a is true")
@@ -21,49 +48,67 @@ else
 end
 
 
-
--- Loop
-a = 0
-b = 5
-
-while (a < b)
-do
-    print("loop count " .. a)
-    a = a + 1
+-- Loop --
+for i=10, 1, -1 -- for init, max/min value, increment
+do 
+   print("loop count " .. i) 
 end
 
 
+-- Conditional loops
+i = 0
+j = 5
+while (i < j)
+do
+    print("loop count " .. i)
+    i = i + 1
+end
+repeat
+    print("loop count " .. i)
+    i = i - 1
+until (i == 0)
 
--- File I/O
 
+-- Function --
+function add(a, b)
+    return (a + b)
+end
+print("10 + 10 = " .. add(10,10))
+
+
+-- Array --
+array = {"duck", 42, "dog"}
+print(array[1])
+print(array[2])
+print(array[3])
+
+
+-- File I/O --
 file = io.open("test.txt", "a")
 io.output(file)
-io.write("This is the end\n")
+io.write("Hello lxi-tools\n")
 io.close(file)
 
 
-
--- String/number conversion
-
+-- String to number conversion
 a_string = "+5.111E+02"
-a_float = tonumber(a_string)
-print("a_float = " .. a_float)
-print("a_float * 2 = " .. a_float * 2)
-
+a_number = tonumber(a_string)
+print("a_number = " .. a_number)
+print(type(a_string))
+print(type(a_number))
 
 
 -- Math
-
 print("2*2 = " .. 2 * 2)
 print("2^4 = " .. math.pow(2,4))
 print("sin(2) = " .. math.sin(2))
 print("sqrt(16) = " .. math.sqrt(16))
 print("pi = " .. math.pi)
-
+print("a_number * 2 = " .. a_number * 2)
 
 
 -- OS functions
-
-os.execute ("echo 'Hello world'")
+os.execute ("echo 'Hello lxi-tools'")
 print(os.date("The time is %X"))
 print("TERM = " .. os.getenv("TERM"))
+
