8e248ba262900411431c3a1a5c40f7a9f93c9ce1
Martin Lund <martin.lund@keep-it-simple.com>
2022-01-27 10:56:07 +0100

    Update multi device example script

--- test/lua/psu-data-log.lua
+++ test/lua/psu-data-log.lua
@@ -3,7 +3,7 @@
 --    https://lxi-tools.github.io  --
 -------------------------------------
 
--- Example: Data logging from an NGM202 PSU for 100 seconds at 10 Hz
+-- Example: Data logging from a NGM202 PSU for 100 seconds at 10 Hz
 
 -- Connect to power supply
 psu = connect("192.168.0.107", nil, nil, 2000, "VXI11")
@@ -37,9 +37,13 @@ do
    msleep(100)
 end
 
+-- Save data
+chart_save_csv(chart, "chart0.csv")
+chart_save_png(chart, "chart0.png")
+
 -- Cleanup
-clock_free(c0)
---chart_close(chart0)
+clock_free(clock0)
+chart_close(chart0)
 
 -- Turn off power supply
 scpi(psu, "output off")
