10f834c525185ca5220d1f976cdcbe48cb1b0c90
Martin Lund <martin.lund@keep-it-simple.com>
2022-02-14 04:07:41 +0100

    Add automation test script (draft)

---
+++ test/lua/test-automation.lua
@@ -0,0 +1,38 @@
+-------------------------------------
+--  lxi-tools                      --
+--    https://lxi-tools.github.io  --
+-------------------------------------
+
+-- Automation test
+
+
+
+-- tc "Test case 1 description"
+
+print("This is test case 1")
+
+
+
+-- tc "Test case 2 description"
+
+print("This is test case 2")
+
+fail()
+
+
+
+-- tc "Test case 3 description"
+
+print("This is test case 3")
+
+fail_stop()
+
+
+
+-- tc "Test case 4 description"
+
+print("This is test case 4")
+
+
+
+-- tc save test-results.log
