6e9f8e8c30dc1e57b83f9e529823b459c827a040
Martin Lund <martin.lund@keep-it-simple.com>
2022-02-16 14:55:45 +0100

    Update PSU data log test script

--- test/lua/psu-data-log.lua
+++ test/lua/psu-data-log.lua
@@ -3,7 +3,7 @@
 --    https://lxi-tools.github.io  --
 -------------------------------------
 
--- Example: Data logging from a NGM202 PSU for 100 seconds at 10 Hz
+-- Example: Data logging from a NGM202 PSU for 25 seconds at 10 Hz
 
 -- Connect to power supply
 psu = connect("192.168.0.107", nil, nil, 2000, "VXI11")
@@ -15,25 +15,25 @@ scpi(psu, "voltage 5.0, (@1)")
 -- Turn on power supply
 scpi(psu, "output on")
 
--- Wait for voltage to stabilize
-msleep(1000)
+-- Wait for power to stabilize
+msleep(100)
 
 -- Setup line chart and clock
-chart0 = chart_new("line-chart",             -- chart type
+chart0 = chart_new("line",                   -- chart type
                    "PSU Channel 1 Data Log", -- title
                    "Time [ s ]",             -- x-axis label
-                   "Voltage [ V ]",          -- y-axis label
-                   100, 10, 800)             -- x max, y max, window width
+                   "Current [ I ]",          -- y-axis label
+                   25, 1, 800)               -- x max, y max, window width
 clock0 = clock_new()
 
--- Capture and plot samples at 10 Hz for 100 seconds
+-- Capture and plot samples at 10 Hz for 25 seconds
 clock = 0
-while (clock < 100)
+while (clock < 25)
 do
-   voltage = scpi(psu, "voltage? (@1)")
-   voltage = tonumber(voltage)
+   current = scpi(psu, "measure:current? (@1)")
+   current = tonumber(current)
    clock = clock_read(clock0)
-   chart_plot(chart0, clock, voltage)
+   chart_plot(chart0, clock, current)
    msleep(100)
 end
 
