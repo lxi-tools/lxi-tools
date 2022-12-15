3cdc15b85a45b12389bcbd0cd6a3a4f8e39d9918
Martin Lund <martin.lund@keep-it-simple.com>
2022-12-16 00:17:03 +0100

    Add code for backwards compatibility with lua 5.0/5.1

--- src/lxi_gui-window.c
+++ src/lxi_gui-window.c
@@ -2699,6 +2699,25 @@ static void lua_line_hook(lua_State *L, lua_Debug *ar)
     }
 }
 
+#if !defined LUA_VERSION_NUM || LUA_VERSION_NUM==501
+/*
+** Adapted from Lua 5.2.0 (for backwards compatibility)
+*/
+static void luaL_setfuncs (lua_State *L, const luaL_Reg *l, int nup)
+{
+  luaL_checkstack(L, nup+1, "too many upvalues");
+  for (; l->name != NULL; l++) {  /* fill the table with given functions */
+    int i;
+    lua_pushstring(L, l->name);
+    for (i = 0; i < nup; i++)  /* copy upvalues to the top */
+      lua_pushvalue(L, -(nup+1));
+    lua_pushcclosure(L, l->func, nup);  /* closure with those upvalues */
+    lua_settable(L, -(nup + 3));
+  }
+  lua_pop(L, nup);  /* remove upvalues */
+}
+#endif
+
 extern int lua_register_gui(lua_State *L)
 {
     // Register gui functions
