0d6c4eda655750834b563b7049a8efcd41ec5ee9
Martin Lund <martin.lund@keep-it-simple.com>
2023-07-23 10:38:00 +0200

    Update postinstall.py to use gtk4-update-icon-cache
    Use gtk4-update-icon-cache instead of gtk-update-icon-cache which is
    only for gtk3.

--- build-aux/meson/postinstall.py
+++ build-aux/meson/postinstall.py
@@ -10,7 +10,7 @@ destdir = environ.get('DESTDIR', '')
 # Package managers set this so we don't need to run
 if not destdir:
     print('Updating icon cache...')
-    call(['gtk-update-icon-cache', '-qtf', path.join(datadir, 'icons', 'hicolor')])
+    call(['gtk4-update-icon-cache', '-qtf', path.join(datadir, 'icons', 'hicolor')])
 
     print('Updating desktop database...')
     call(['update-desktop-database', '-q', path.join(datadir, 'applications')])
