36bc0a18bc2b6df8ec736698b48fb2c53421690a
Martin Lund <martin.lund@keep-it-simple.com>
2024-11-06 22:01:32 +0100

    Fix version string

--- version.sh
+++ version.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 if [ "$1" = "get-vcs" ]; then
-  git -C "$MESON_SOURCE_ROOT" describe --always --dirty
+  git -C "$MESON_SOURCE_ROOT" describe --always --dirty | sed 's/^v//'
 elif [ "$1" = "set-dist" ]; then
   $MESONREWRITE --sourcedir="$MESON_PROJECT_DIST_ROOT" kwargs set project / version "$2"
 else
